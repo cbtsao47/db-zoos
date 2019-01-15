522998ca31ddd68cbb2b8a805536bcb82bbdc302
wisdomtooth <cbtsao47@gmail.com>
2019-01-15 16:33:43 -0600

    add error messages

--- routes/bearsRouter.js
+++ routes/bearsRouter.js
@@ -8,8 +8,11 @@ const route = express.Router();
 
 route.get("/", async (req, res) => {
   try {
-    const result = await db("bears");
-    res.json(result);
+    const result = await db("zoos");
+    if (result.length > 0) {
+      res.json(result);
+    }
+    res.status(404).json({ message: `Data Not Found` });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,43 +21,61 @@ route.get("/", async (req, res) => {
 route.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await db("bears").where({ id });
-    res.json(result);
+    const result = await db("zoos").where({ id });
+    if (result.length > 0) {
+      res.json(result[0]);
+    }
+    res.status(404).json({ message: "Data Not Found" });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 route.post("/", async (req, res) => {
   try {
-    const result = await db("bears").insert(req.body);
-    res.status(201).json(result);
+    const result = await db("zoos").insert(req.body);
+    if (result.name.length > 0) {
+      res.status(201).json({
+        message: `The data has been created with the id of ${result}`
+      });
+    }
+    res.status(406).json({ message: "Please include a name!" });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
-
 route.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await db("bears")
-      .where({ id })
-      .del();
-    res.json(result);
+    const check = await db("bears").where({ id });
+    if (check) {
+      const result = await db("bears")
+        .where({ id })
+        .del();
+      res.status(202).json({ message: `Data has been deleted` });
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 route.put("/:id", async (req, res) => {
   const { id } = req.params;
   const change = req.body;
   try {
-    const result = await db("bears")
-      .where({ id })
-      .update(change);
-    res.json(result);
+    const check = await db("bears").where({ id });
+    if (check) {
+      const result = await db("bears")
+        .where({ id })
+        .update(change);
+      res.status(202).json({ message: "Data has been updated" });
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
--- routes/zoosRouter.js
+++ routes/zoosRouter.js
@@ -8,16 +8,11 @@ const db = knex(knexConfig.development);
 
 route.get("/", async (req, res) => {
   try {
-    res.json(await db("zoos"));
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-route.post("/", async (req, res) => {
-  try {
-    const result = await db("zoos").insert(req.body);
-    res.json(result);
+    const result = await db("zoos");
+    if (result.length > 0) {
+      res.json(result);
+    }
+    res.status(404).json({ message: `Data Not Found` });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -27,20 +22,42 @@ route.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const result = await db("zoos").where({ id });
-    res.json(result);
+    if (result.length > 0) {
+      res.json(result[0]);
+    }
+    res.status(404).json({ message: "Data Not Found" });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+route.post("/", async (req, res) => {
+  try {
+    const result = await db("zoos").insert(req.body);
+    if (result.name.length > 0) {
+      res.status(201).json({
+        message: `The data has been created with the id of ${result}`
+      });
+    }
+    res.status(406).json({ message: "Please include a name!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 route.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await db("zoos")
-      .where({ id })
-      .del();
-    res.json(result);
+    const check = await db("zoos").where({ id });
+    if (check) {
+      const result = await db("zoos")
+        .where({ id })
+        .del();
+      res.status(202).json({ message: `Data has been deleted` });
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
@@ -48,12 +65,17 @@ route.put("/:id", async (req, res) => {
   const { id } = req.params;
   const change = req.body;
   try {
-    const result = await db("zoos")
-      .where({ id })
-      .update(change);
-    res.json(result);
+    const check = await db("zoos").where({ id });
+    if (check) {
+      const result = await db("zoos")
+        .where({ id })
+        .update(change);
+      res.status(202).json({ message: "Data has been updated" });
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
